f4d0dcb75729f3eb532a4d4cd6ff45e06cd21cd0
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-03 14:21:20 +0530

    JavaScript program to print table of a number (Iterative approach)

---
+++ Logic Building Problems/Basic Problems/Program to print multiplication table of a number/logic1.js
@@ -0,0 +1,13 @@
+// Javascript program to print
+// table of a number
+
+function printTable(n) { 
+    for (let i = 1; i <= 10; ++i)
+        console.log( n + " * " +i +
+                " = " + n *
+                    i);
+    }
+    
+    // Driver Code
+    let n = 5; 
+    printTable(n);
