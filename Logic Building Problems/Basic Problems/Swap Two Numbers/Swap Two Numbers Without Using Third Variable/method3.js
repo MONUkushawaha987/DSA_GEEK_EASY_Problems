15417f1aac56d7f6f03bf3a3bc560e26c83f26dc
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-04 13:42:42 +0530

    JavaScript Code to swap two numbers

---
+++ Logic Building Problems/Basic Problems/Swap Two Numbers/Swap Two Numbers Without Using Third Variable/method3.js
@@ -0,0 +1,9 @@
+// JavaScript Code to swap two numbers using destructuring assignment
+
+let a = 2, b = 3;
+console.log("a = " + a + " b = " + b);   
+
+// destructuring assignment
+[a, b] = [b, a]
+
+console.log("a = " + a + " b = " + b);
