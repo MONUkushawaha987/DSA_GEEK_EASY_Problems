7b172ff34830d4c32cb8be0bd2c067dcc4f15388
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-04 13:41:40 +0530

    C++ Code to swap two numbers using swap function

---
+++ Logic Building Problems/Basic Problems/Swap Two Numbers/Swap Two Numbers Without Using Third Variable/method3.cpp
@@ -0,0 +1,14 @@
+// C++ Code to swap two numbers using swap function
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2, b = 3;
+    cout << "a = " << a << " b = " << b << endl;   
+  
+    swap(a, b);
+  
+    cout << "a = " << a << " b = " << b << endl;
+    return 0;
+}
