cbb99c8ad35595d456ca9550bf91ae086d262256
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-04 13:20:46 +0530

    JavaScript Code to swap two numbers using third variable

---
+++ Logic Building Problems/Basic Problems/Swap Two Numbers/method1.js
@@ -0,0 +1,11 @@
+// JavaScript Code to swap two numbers using third variable
+
+let a = 2, b = 3;
+console.log("a = " + a + " b = " + b);   
+
+// Swap a and b using temp variable
+let temp = a;
+a = b;
+b = temp;
+
+console.log("a = " + a + " b = " + b);
