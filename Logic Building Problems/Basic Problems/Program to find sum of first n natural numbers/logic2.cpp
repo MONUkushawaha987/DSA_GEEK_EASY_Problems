9f26b365c8b1567aafde9b021e377ab97362dde1
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-04 13:03:50 +0530

    An efficient solution is to use the formula (CPP Program)

---
+++ Logic Building Problems/Basic Problems/Program to find sum of first n natural numbers/logic2.cpp
@@ -0,0 +1,17 @@
+// Efficient CPP program to find sum of first
+// n natural numbers.
+#include<iostream>
+using namespace std;
+
+int findSum(int n)
+{
+   return n * (n + 1) / 2;
+}
+
+// Driver code
+int main()
+{
+  int n = 5;
+  cout << findSum(n);
+  return 0;
+}
