570436494ee6b31b7df0d8e57bd22b4fa80f5cc6
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-06 13:30:03 +0530

    java program for the above approach (using sum of two sides

---
+++ Logic Building Problems/Basic Problems/The dice problem/method2.java
@@ -0,0 +1,19 @@
+// Java program for the above approach
+import java.io.*;
+
+class Main{
+  
+    static void oppositeFaceOfDice(int n)  {
+      
+        // Stores number on opposite face
+        // of dice
+        int ans = 7 - n;
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 2;
+        oppositeFaceOfDice(n);
+    }
+}
