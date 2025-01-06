d90dee775dc8d81fb9dff94a3bef15018d43abb4
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-06 13:31:00 +0530

    javascript program for the above approach (using sum of two sides)

---
+++ Logic Building Problems/Basic Problems/The dice problem/method2.js
@@ -0,0 +1,12 @@
+// JavaScript program for the above approach
+
+function oppositeFaceOfDice(n) {
+    // Stores number on opposite face
+    // of dice
+    let ans = 7 - n;
+    console.log(ans);
+  }
+  
+  // Driver Code
+  let n = 2;
+  oppositeFaceOfDice(n);
