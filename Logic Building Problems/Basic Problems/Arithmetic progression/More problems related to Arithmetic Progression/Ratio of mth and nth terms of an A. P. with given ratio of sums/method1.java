51505043d39c74512a700c23c004a749217c8e65
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-11 12:18:22 +0530

    Ratio of mth and nth terms of an A. P. in java

---
+++ Logic Building Problems/Basic Problems/Arithmetic progression/More problems related to Arithmetic Progression/Ratio of mth and nth terms of an A. P. with given ratio of sums/method1.java
@@ -0,0 +1,24 @@
+// Java code to calculate ratio
+import java.io.*;
+
+class Nth {
+	
+// function to calculate ratio of mth and nth term
+static float CalculateRatio(float m, float n)
+{
+	// ratio will be tm/tn = (2*m - 1)/(2*n - 1)
+	return (2 * m - 1) / (2 * n - 1);
+}
+}
+
+// Driver code
+class Main {
+	
+	public static void main (String[] args) {
+	float m = 6, n = 2;
+	Nth a=new Nth();
+System.out.println(a.CalculateRatio(m, n));
+
+	}
+}
+
