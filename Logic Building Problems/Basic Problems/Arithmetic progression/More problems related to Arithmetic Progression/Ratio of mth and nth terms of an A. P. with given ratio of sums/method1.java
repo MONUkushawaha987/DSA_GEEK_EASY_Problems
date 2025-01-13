4db9761c387e3b447582d88516092037475dfb9a
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-13 15:25:40 +0530

    C++ program to print all triplets in given C++ program to print all triplets in given array that form Arithmetic Progression array that form Arithmetic Progression

--- Logic Building Problems/Basic Problems/Arithmetic progression/More problems related to Arithmetic Progression/Ratio of mth and nth terms of an A. P. with given ratio of sums/method1.java
+++ Logic Building Problems/Basic Problems/Arithmetic progression/More problems related to Arithmetic Progression/Ratio of mth and nth terms of an A. P. with given ratio of sums/method1.java
@@ -1,10 +1,10 @@
 // Java code to calculate ratio
-import java.io.*;
+// import java.io.*;
 
 class Nth {
 	
 // function to calculate ratio of mth and nth term
-static float CalculateRatio(float m, float n)
+float CalculateRatio(float m, float n)
 {
 	// ratio will be tm/tn = (2*m - 1)/(2*n - 1)
 	return (2 * m - 1) / (2 * n - 1);
@@ -17,7 +17,7 @@ class Main {
 	public static void main (String[] args) {
 	float m = 6, n = 2;
 	Nth a=new Nth();
-System.out.println(a.CalculateRatio(m, n));
+	System.out.println(a.CalculateRatio(m, n));
 
 	}
 }
