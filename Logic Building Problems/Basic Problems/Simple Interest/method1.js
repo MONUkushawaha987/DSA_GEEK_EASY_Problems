0eb7d217f5957684041952c12286eaf41d99aaee
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-18 12:40:45 +0530

    program to find simple interest for given principal amount, time and rate of interest in javascript

---
+++ Logic Building Problems/Basic Problems/Simple Interest/method1.js
@@ -0,0 +1,8 @@
+function simpleInterest(p, t, r) {
+    /* Calculate simple interest  */
+    return (p * t * r) / 100;
+}
+
+//driver code
+let p = 1, r = 1, t = 1;
+console.log(simpleInterest(p, r, t));
