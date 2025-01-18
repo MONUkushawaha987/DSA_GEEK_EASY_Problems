c1cc7e9e14735074441f39ef7a08a0a0e5063723
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-18 12:39:20 +0530

    CPP program to find simple interest for given principal amount, time and rate of interest

---
+++ Logic Building Problems/Basic Problems/Simple Interest/method1.cpp
@@ -0,0 +1,18 @@
+// CPP program to find simple interest for
+// given principal amount, time and rate of interest
+#include<iostream>
+using namespace std;
+
+float simpleInterest(float p, float t, float r){
+    /* Calculate simple interest  */
+    float ans = (p * t * r) / 100;
+    return ans;
+}
+
+int main() {
+
+    float p = 1, r = 1, t = 1;
+    cout <<simpleInterest(p,r,t);
+
+    return 0;
+}
