bcb95bd113b9f287c8bbfebc8beca5a93d5015a9
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-06 13:40:57 +0530

    program to find GCD of two numbers in java

---
+++ Logic Building Problems/Easy Problems/Program to Find GCD or HCF of Two Numbers/Main.java
@@ -0,0 +1,29 @@
+// Java program to find GCD of two numbers
+import java.io.*;
+
+public class Main {
+
+    // Function to return gcd of a and b
+    static int gcd(int a, int b)
+    {
+        // Find Minimum of a and b
+        int result = Math.min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+
+        // Return gcd of a and b
+        return result;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int a = 98, b = 56;
+        System.out.print("GCD of " + a + " and " + b
+                         + " is " + gcd(a, b));
+    }
+}
