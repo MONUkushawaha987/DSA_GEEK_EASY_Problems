841b7a520a145b4fc69bf705bc3db6f138337422
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-07 17:33:05 +0530

    program to find gcd using inbuilt functions in java

---
+++ Logic Building Problems/Easy Problems/Program to Find GCD or HCF of Two Numbers/GFG.java
@@ -0,0 +1,20 @@
+// JAVA program to find gcd using inbuilt functions
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 98, b = 56;
+        int gcd = gcd(a, b);
+        System.out.println("The gcd of a and b is " + gcd);
+    }
+
+    public static int gcd(int a, int b)
+    {
+        BigInteger bigA = BigInteger.valueOf(Math.abs(a));
+        BigInteger bigB = BigInteger.valueOf(Math.abs(b));
+        BigInteger gcd = bigA.gcd(bigB);
+        return gcd.intValue();
+    }
+}
