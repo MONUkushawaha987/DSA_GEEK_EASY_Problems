d23472b4e027371e97296ac21d65e942dc265489
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-07 17:32:20 +0530

    program to find gcd using inbuilt functions in cpp

---
+++ Logic Building Problems/Easy Problems/Program to Find GCD or HCF of Two Numbers/method6.cpp
@@ -0,0 +1,11 @@
+// c++ program to find gcd using inbuilt functions
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 98, b = 56;
+    cout << "The gcd of a and b is " << __gcd(a, b) << endl;
+    return 0;
+}
