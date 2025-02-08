23152a145ab15968c9dc970bf22a59ce62333e02
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-08 12:08:24 +0530

    program to find LCM of two numbers in javascript

---
+++ Logic Building Problems/Easy Problems/Program to find LCM of two numbers/method1.js
@@ -0,0 +1,24 @@
+
+
+// Javascript program to find LCM of two numbers 
+
+// Recursive function to return gcd of a and b 
+function gcd(a, b) 
+{ 
+if (b == 0) 
+    return a; 
+return gcd(b, a % b); 
+} 
+
+// Function to return LCM of two numbers 
+function lcm(a, b) 
+{ 
+    return (a / gcd(a, b)) * b; 
+} 
+
+// Driver program to test above function 
+ 
+    let a = 15, b = 20; 
+    document.write("LCM of " + a + " and "
+    + b + " is " + lcm(a, b)); 
+    
